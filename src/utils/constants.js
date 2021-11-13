bac7a433264969411a5f3092f68c51e439c00e9f
LeoFC97 <leonardo.fc2@outlook.com>
2021-11-13 02:29:31 -0300

    feat: add contractAddress for bcoin and ccar

---
+++ src/utils/constants.js
@@ -0,0 +1,12 @@
+module.exports={
+    listOfContracts: [
+        {
+            coin: 'bcoin',
+            contractAddress: '0x00e1656e45f18ec6747f5a8496fd39b50b38396d',
+        },
+        {
+            coin: 'ccar',
+            contractAddress: '0x50332bdca94673f33401776365b66cc4e81ac81d',
+        },
+    ]
+}
