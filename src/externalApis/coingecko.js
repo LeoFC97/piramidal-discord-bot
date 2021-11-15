e59d941b94ef87546320ac6a738c9350aa1d9e9b
LeoFC97 <leonardo.fc2@outlook.com>
2021-11-15 15:50:41 -0300

    fix: obj acess

--- src/externalApis/coingecko.js
+++ src/externalApis/coingecko.js
@@ -9,15 +9,15 @@ module.exports={
     if(!contract){
       return false;
     }
-    const htmlParsed = await axios.get(
-        `${coingecko.baseUrl}/simple/token_price/binance-smart-chain/?contract_addresses=${contract}&vs_currencies=${currencies}`
+    const coinPice = await axios.get(
+        `${coingecko.baseUrl}/simple/token_price/binance-smart-chain/?contract_addresses=${contract.contractAddress}&vs_currencies=${currencies}`
     ).then((response) => {
-      return response.data[contract];
+      return response.data[contract.contractAddress];
     })
     .catch((error) => {
       console.log(error)
       return false;
     })
-    return htmlParsed
+    return coinPice;
   },
 }
